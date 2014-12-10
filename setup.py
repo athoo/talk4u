8626995432b73f790aad62e1ea4090086dc20d93
athoo <athoo@sina.com>
2014-12-10 13:53:28 +0800

    structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='talk4u',
+      version='0.1',
+      description='This app help you to listen to the world',
+      url='https://github.com/athoo/talk4u',
+      author='David Yang',
+      author_email='pingjing.yang@gmail.com',
+      license='MIT',
+      packages=['talk4u'],
+      zip_safe=False)
